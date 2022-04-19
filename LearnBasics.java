45a20f4bdb2165e7ba93187582d4adc15734367f
Pepronia <55874942+Pepronia@users.noreply.github.com>
2022-04-19 12:20:36 +0100

    Add files via upload

---
+++ LearnBasics.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class LearnBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hi");
+        
+        Boolean car = true;
+        Byte petrol = 45;
+        char wheels = 4;
+        long configuration = 589L;
+        float engineNumber = 45.567F;
+        short distant = 5000;
+        int seats = 7;
+        double idNumber = 786.453234242;
+        
+        
+        
+        
+        
+        
+        
+        
+	}
+
+}
